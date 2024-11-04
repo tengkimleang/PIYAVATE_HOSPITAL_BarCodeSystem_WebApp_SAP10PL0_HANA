c1f57ea2c45040d226eff952137781ba203523d4
TengKimleang <Nonametotell4@gmail.com>
2024-11-04 17:28:54 +0700

    Add

--- FrontEnd/V2/Piyavate_Hospital.Shared/Services/ApiConstant.cs
+++ FrontEnd/V2/Piyavate_Hospital.Shared/Services/ApiConstant.cs
@@ -5,6 +5,6 @@ public class ApiConstant
     public static readonly string ApiUrl = "http://192.168.20.2:8082";
     // public static readonly string ApiUrl = "http://localhost:5253";
     // public static readonly string ApiUrl = "http://localhost:8082";
-    // public static readonly string ApiBase = "http://localhost:5121";
-    public static readonly string ApiBase = "http://192.168.20.2:8081";
+    public static readonly string ApiBase = "http://localhost:5121";
+    // public static readonly string ApiBase = "http://192.168.20.2:8081";
 }
