d2aa537969b23bc4cda9e15fafaf9b96c42b98d0
TKL <Nonametotell4@gmail.com>
2024-11-07 17:12:19 +0700

    Add

--- FrontEnd/V2/Piyavate_Hospital.Shared/Services/ApiConstant.cs
+++ FrontEnd/V2/Piyavate_Hospital.Shared/Services/ApiConstant.cs
@@ -5,7 +5,7 @@ public class ApiConstant
     public static readonly string ApiUrl = "http://1.10.143.138:8429/Piyavate/API";
     // public static readonly string ApiUrl = "http://localhost:5253";
     // public static readonly string ApiUrl = "http://localhost:8082";
-    // public static readonly string ApiBase = "http://localhost:5121";
+    public static readonly string ApiBase = "http://localhost:5121";
     public static readonly string StoreProcedure = "_USP_CALLTRANS_TRANSACTION";
-    public static readonly string ApiBase = "http://192.168.10.107:8429/Piyavate/BarCodeSystem";
+    // public static readonly string ApiBase = "http://192.168.10.107:8429/Piyavate/BarCodeSystem";
 }
